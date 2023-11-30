40638a4b669b01c61626e3ba15ba88b3fa40e05f
Krzychu <krzysztofkwiatkowski555@gmail.com>
2023-11-30 09:03:49 +0100

    save

--- Studies/OOP/ATM.cpp
+++ Studies/OOP/ATM.cpp
@@ -15,14 +15,6 @@ struct car {
 
 
 int main() {
-    car audi_RS6{ "Audi", "RS6", 400000, 3.29, "KR 82012", "Sport" };
-    car audi_A8{ "Audi", "A8", 150000, 2.40, "KK 82762", "Combi" };
-    car mercedes_cla200{ "Mercedes", "CLA 200 AMG", 400000, 4.29, "KR 76404", "Sport" };
-    car tesla_model_s{ "Tesla", "Model S", 800000, 2.20, "EL 12345", "Electric" };
-    car chevrolet_camaro{ "Chevrolet", "Camaro", 500000, 4.50, "CC 54321", "Convertible" };
-    car nissan_altima{ "Nissan", "Altima", 300000, 2.90, "NA 98765", "Sedan" };
-    car jaguar_f_type{ "Jaguar", "F-Type", 600000, 5.20, "JF 23456", "Coupe" };
-    car subaru_outback{ "Subaru", "Outback", 350000, 3.00, "SO 87654", "SUV" };
 
     int choice;
 
