f3693c2d639c5db653158369123d59dc0f84ca42
Krzychu <krzysztofkwiatkowski555@gmail.com>
2023-12-07 10:08:29 +0100

    save

--- car_rental_menu.cpp
+++ car_rental_menu.cpp
@@ -2,39 +2,52 @@
 #include <string>
 using namespace std;
 
-struct car {
-    string brand;
-    string model;
-    double price;
-    double price_per_kilometer;
-    string license_plate;
-    string type;
-};
+int calculateS(int arr[]) {
+    int sum = 0;
+    for (int i = 0; i < 6; ++i) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+//int smallest(int arr[]) {
+//    int smallest = arr[0];
+//    for (int i = 1; i < 5; ++i) {
+//        if 
+//    }
+//}
+
+void findMinMax(const int arr[]) {
+    minElement = arr[0];
+    maxElement = arr[0];
+
+    for (int i = 1; i < 6; ++i) {
+        if (arr[i] < minElement) {
+            minElement = arr[i];
+        }
+        if (arr[i] > maxElement) {
+            maxElement = arr[i];
+        }
+    }
+}
 
 
-int main() {
-    car audi_RS6{"Audi", "RS6", 400000, 3.29, "KR 82012", "Sport"};
-    car audi_A8{"Audi", "A8", 150000, 2.40, "KK 82762", "Combi"};
-    car mercedes_cla200{"Mercedes", "CLA 200 AMG", 400000, 4.29, "KR 76404", "Sport"};
-    car tesla_model_s{"Tesla", "Model S", 800000, 2.20, "EL 12345", "Electric"};
-    car chevrolet_camaro{"Chevrolet", "Camaro", 500000, 4.50, "CC 54321", "Convertible"};
-    car nissan_altima{"Nissan", "Altima", 300000, 2.90, "NA 98765", "Sedan"};
-    car jaguar_f_type{"Jaguar", "F-Type", 600000, 5.20, "JF 23456", "Coupe"};
-    car subaru_outback{"Subaru", "Outback", 350000, 3.00, "SO 87654", "SUV"};
 
-    do
-    {
-        system("cls");
+int main()
+{
+    int main() {
+        int array1[6] = { 5, 3, 8, 2, 7, 1 };
+        int array2[6] = { 10, 20, 5, 15, 25, 0 };
+        int array3[6] = { 9, 4, 7, 2, 6, 1 };
 
-        cout << endl << "*** Welcome to our car rental services ***";
-        cout << endl << "Select options:";
-        cout << endl << "1. Check available cars to rent";
-        cout << endl << "2. Login to your account";
-        cout << endl << "  ﹂ Check your balance";
-        cout << endl << "  ﹂ Show your last rides";
-        cout << endl << "... more in future";
-    } while (1);
-    
 
+        cout << "Sum of array: " << calculateSum(array1) << endl;
+        cout << "Sum of array2: " << calculateSum(array2) << endl;
+        cout << "Sum of array3: " << calculateSum(array3) << endl;
+
+        int minElement, maxElement;
+        findMinMax(array size, minElement, maxElement);
+        cout << "Min Element of array: " << minElement << endl;
+        cout << "Max Element of array: " << maxElement << endl;
     return 0;
 }
